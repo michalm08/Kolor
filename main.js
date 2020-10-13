46b6a937ff09f2074f1b516852fb7e89c814b8a7
michalm08 <44162147+michalm08@users.noreply.github.com>
2020-10-14 00:21:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,129 @@
+const green="rgba(70, 160, 92, 0.70)";
+const blue="rgba(70, 100, 160, 0.72)";
+const pink="rgba(189, 111, 192, 0.569)";
+const red="rgba(184, 78, 78, 0.569)";
+
+
+switch(new Date().getHours())
+{
+case 0:
+  color=green;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  
+  break;
+
+case 1:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 2:
+  color=pink;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 3:
+  color=red;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 4:
+  color=green;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 5:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 6:
+  color=pin;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 7:
+  color=red;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 8:
+  color=green;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 9:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 10:
+  color=pink;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 11:
+  color=red;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 12:
+  color=green;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 13:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 14:
+  color=pink;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 15:
+  color=red;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 16:
+  color=green;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 17:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 18:
+  color=pink;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 19:
+  color=red;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 20:
+  color=green;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 21:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 22:
+  color=pink;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+
+case 23:
+  color=blue;
+  document.querySelector('.myBox').style.backgroundColor=color;
+  break;
+}
